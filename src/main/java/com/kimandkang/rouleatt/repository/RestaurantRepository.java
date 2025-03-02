14c5b8528ead5e16367956d5c140ce9c2b5b5c87
KimChanJin97 <cjkimhello97@gmail.com>
2025-03-02 14:33:30 +0900

    feat: findNearbyRestaurants() 구현

---
+++ src/main/java/com/kimandkang/rouleatt/repository/RestaurantRepository.java
@@ -0,0 +1,25 @@
+package com.kimandkang.rouleatt.repository;
+
+import com.kimandkang.rouleatt.domain.Restaurant;
+import java.util.List;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RestaurantRepository extends CrudRepository<Restaurant, Long> {
+
+    @Query(value = """
+                SELECT *
+                FROM restaurant r
+                WHERE ST_Contains(
+                    (ST_Buffer(ST_GeomFromText(CONCAT('POINT(', :y, ' ', :x, ')'), 4326), :distance)),
+                    r.coordinate)
+            """, nativeQuery = true)
+    List<Restaurant> findNearbyRestaurants(
+            @Param("x") double x,
+            @Param("y") double y,
+            @Param("distance") double distance
+    );
+}
