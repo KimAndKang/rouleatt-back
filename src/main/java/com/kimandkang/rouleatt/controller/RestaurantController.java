7238d71884063b45bd1ab1eb59e627eef87d4a6d
KimChanJin97 <cjkimhello97@gmail.com>
2025-03-23 04:53:23 +0900

    feat: 메뉴, 리뷰, 영업시간 반환하도록 컨트롤러, 서비스 구현

--- src/main/java/com/kimandkang/rouleatt/controller/RestaurantController.java
+++ src/main/java/com/kimandkang/rouleatt/controller/RestaurantController.java
@@ -2,8 +2,10 @@ package com.kimandkang.rouleatt.controller;
 
 import com.kimandkang.rouleatt.dto.RestaurantResponses;
 import com.kimandkang.rouleatt.service.RestaurantService;
+import java.time.LocalDateTime;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,8 +22,9 @@ public class RestaurantController {
     public RestaurantResponses findNearbyRestaurants(
             @RequestParam double x,
             @RequestParam double y,
-            @RequestParam(defaultValue = "100", name = "distance") double distance,
-            @RequestParam(required = false, name = "exclude") List<String> exclude
+            @RequestParam(defaultValue = "100") double distance,
+            @RequestParam(required = false) List<String> exclude,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime now
     ) {
         // 거리가 벗어날 경우
         if (distance < 100 || distance > 1000) {
@@ -29,9 +32,9 @@ public class RestaurantController {
         }
         // 제외할 카테고리가 존재하지 않을 경우
         if (exclude == null || exclude.isEmpty()) {
-            return restaurantService.findNearbyRestaurantsWithoutExclude(x, y, distance);
+            return restaurantService.findNearbyRestaurantsWithoutExclude(x, y, distance, now);
         }
         // 제외할 카테고리가 존재할 경우
-        return restaurantService.findNearbyRestaurantsWithExclude(x, y, distance, exclude);
+        return restaurantService.findNearbyRestaurantsWithExclude(x, y, distance, exclude, now);
     }
 }
--- src/main/java/com/kimandkang/rouleatt/service/RestaurantService.java
+++ src/main/java/com/kimandkang/rouleatt/service/RestaurantService.java
@@ -3,6 +3,7 @@ package com.kimandkang.rouleatt.service;
 import com.kimandkang.rouleatt.domain.Restaurant;
 import com.kimandkang.rouleatt.dto.RestaurantResponses;
 import com.kimandkang.rouleatt.repository.RestaurantRepository;
+import java.time.LocalDateTime;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,14 +16,25 @@ public class RestaurantService {
     private final RestaurantRepository restaurantRepository;
 
     @Transactional(readOnly = true)
-    public RestaurantResponses findNearbyRestaurantsWithoutExclude(double x, double y, double distance) {
+    public RestaurantResponses findNearbyRestaurantsWithoutExclude(
+            double x,
+            double y,
+            double distance,
+            LocalDateTime now
+    ) {
         List<Restaurant> restaurants = restaurantRepository.findNearbyRestaurantsWithoutExclude(x, y, distance);
-        return RestaurantResponses.from(restaurants);
+        return RestaurantResponses.from(restaurants, now);
     }
 
     @Transactional(readOnly = true)
-    public RestaurantResponses findNearbyRestaurantsWithExclude(double x, double y, double distance, List<String> exclude) {
+    public RestaurantResponses findNearbyRestaurantsWithExclude(
+            double x,
+            double y,
+            double distance,
+            List<String> exclude,
+            LocalDateTime now
+    ) {
         List<Restaurant> restaurants = restaurantRepository.findNearbyRestaurantsWithExclude(x, y, distance, exclude);
-        return RestaurantResponses.from(restaurants);
+        return RestaurantResponses.from(restaurants, now);
     }
 }
