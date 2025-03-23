3e13c354f9e351a429e931b3b632fa7f05c6b157
KimChanJin97 <cjkimhello97@gmail.com>
2025-03-23 21:57:55 +0900

    docs: 스웨거 적용

--- src/main/java/com/kimandkang/rouleatt/controller/RestaurantController.java
+++ src/main/java/com/kimandkang/rouleatt/controller/RestaurantController.java
@@ -2,6 +2,9 @@ package com.kimandkang.rouleatt.controller;
 
 import com.kimandkang.rouleatt.dto.RestaurantResponses;
 import com.kimandkang.rouleatt.service.RestaurantService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.time.LocalDateTime;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "음식점 조회 컨트롤러")
 @RestController
 @RequestMapping("/restaurants")
 @RequiredArgsConstructor
@@ -19,22 +23,28 @@ public class RestaurantController {
     private final RestaurantService restaurantService;
 
     @GetMapping("/nearby")
+    @Operation(summary = "음식점 조회 메서드")
     public RestaurantResponses findNearbyRestaurants(
+            @Parameter(description = "경도(longitude)", example = "127.0123456789")
             @RequestParam double x,
+            @Parameter(description = "위도(latitude)", example = "37.0123456789")
             @RequestParam double y,
-            @RequestParam(defaultValue = "100") double distance,
+            @Parameter(description = "거리(distance)", example = "1000")
+            @RequestParam(defaultValue = "100") double d,
+            @Parameter(description = "제외할 카테고리", example = "오리요리,생선회")
             @RequestParam(required = false) List<String> exclude,
+            @Parameter(description = "요청 시각", example = "2025-03-22T15:30:00")
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime now
     ) {
         // 거리가 벗어날 경우
-        if (distance < 100 || distance > 1000) {
-            distance = 100;
+        if (d < 100 || d > 1000) {
+            d = 100;
         }
         // 제외할 카테고리가 존재하지 않을 경우
         if (exclude == null || exclude.isEmpty()) {
-            return restaurantService.findNearbyRestaurantsWithoutExclude(x, y, distance, now);
+            return restaurantService.findNearbyRestaurantsWithoutExclude(x, y, d, now);
         }
         // 제외할 카테고리가 존재할 경우
-        return restaurantService.findNearbyRestaurantsWithExclude(x, y, distance, exclude, now);
+        return restaurantService.findNearbyRestaurantsWithExclude(x, y, d, exclude, now);
     }
 }
--- src/main/java/com/kimandkang/rouleatt/dto/RestaurantResponse.java
+++ src/main/java/com/kimandkang/rouleatt/dto/RestaurantResponse.java
@@ -3,9 +3,11 @@ package com.kimandkang.rouleatt.dto;
 import com.kimandkang.rouleatt.domain.Restaurant;
 import com.kimandkang.rouleatt.utils.BizHourUtils;
 import com.kimandkang.rouleatt.utils.MenuUtils;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Schema(description = "음식점 조회 응답 DTO")
 public record RestaurantResponse(
         Long id,
         String name,
@@ -14,7 +16,9 @@ public record RestaurantResponse(
         String category,
         String address,
         String roadAddress,
+        @Schema(description = "영업 여부", example = "`영업 시간 정보가 없어요`, `영업 준비 중`, `영업 종료`, `브레이크 타임`, `영업 중` 문자열 중 하나")
         String isOpen,
+        @Schema(description = "가격대", example = "`4500`, `4500~7500`, `가격 정보 없음` 문자열 형태 중 하나")
         String avgPrice,
         List<MenuResponse> menus,
         List<ReviewResponse> reviews,
